13425a25c8ad5fa552d259e128f43bec7c09250c
Ankit <ankitpunia1274@gmail.com>
2020-06-09 13:01:19 +0530

    Adding two digit using Servlet

---
+++ ServletAdding/src/com/wipro/add/AddServlet.java
@@ -0,0 +1,47 @@
+package com.wipro.add;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		int  a = Integer.parseInt(request.getParameter("first"));
+		int b = Integer.parseInt(request.getParameter("second"));
+		int result=a+b;
+		out.println("<font color=Green>Result= </font>"+result);
+		out.close();
+		}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
